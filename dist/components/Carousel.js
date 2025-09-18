82d0bc67934508fb93200cc3b9a5f073062bfcf0
daniel-table22 <daniel.nacamuli@table22.com>
2025-09-18 13:01:10 -0400

    Remove dist from repo and re-ignore built artifacts

--- dist/components/Carousel.js
+++
@@ -1,6 +0,0 @@
-import { C as t } from "../index-c0afefd0.mjs";
-import "../jsx-runtime-11e331e2.mjs";
-import "react";
-export {
-  t as Carousel
-};
--- dist/components/HowItWorks.js
+++
@@ -1,8 +0,0 @@
-import { H as s } from "../index-f0f3e66c.mjs";
-import "../jsx-runtime-11e331e2.mjs";
-import "react";
-import "./PanelHeader.js";
-import "../ChevronDownIcon-3d5fb7df.mjs";
-export {
-  s as HowItWorks
-};
